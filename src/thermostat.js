038eecc8940418ee43366d47bf59381983ba39b4
Nick <nicholasjohnrichardson@gmail.com>
2018-06-13 18:36:33 +0100

    Thermostat displays energy usage levels

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,7 +5,9 @@ function Thermostat() {
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
   this.MINIMUM_TEMPERATURE = 10;
-  this.temperature = 20;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+  this.temperature = this.DEFAULT_TEMPERATURE;
 };
 
 Thermostat.prototype.getCurrentTemperature = function(){
@@ -45,3 +47,15 @@ Thermostat.prototype.switchPowerSavingModeOff = function () {
 Thermostat.prototype.switchPowerSavingModeOn = function () {
   this.powerSavingMode = true;
 }
+Thermostat.prototype.resetTemperature = function () {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+}
+Thermostat.prototype.energyUsage = function () {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+};
