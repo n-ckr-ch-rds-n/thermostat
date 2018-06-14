1faa08d26dc5b7d0b014c45113e736bce453ae2b
Laura Petre <laura.d.petre@gmail.com>
2018-06-14 17:36:45 +0100

    Implemented current city temperature using Open Weather API

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed'
+    var units = '&units=metric'
+
+    $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  });
+
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
 
